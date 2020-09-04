5831f0b6c545f674e555ee019e7fafb8dc204fac
HiSng <strogalev617@gmail.com>
2020-09-04 21:05:19 +0300

    second commit

--- Forhub/Forhub/Program.cs
+++ Forhub/Forhub/Program.cs
@@ -7,6 +7,7 @@ namespace Forhub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
         }
     }
 }
